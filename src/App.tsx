0d183e2acfd950a2e8d277f6ac6b09abc404594a
Ben Awad <benawad97@gmail.com>
2019-08-23 18:51:05 -0500

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Home } from "./Home";
+import { About } from "./About";
+import { Post } from "./Post";
+
+// setup
+// create routes
+// change routes
+// route params
 
 const App: React.FC = () => {
-  return <div>hey</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/posts/:id" exact component={Post} />
+        <Route path="/" render={() => <div>404</div>} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
